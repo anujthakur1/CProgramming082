328f44ae044206655a1d3a9b6dca2029499d66a9
Rajesh Thapa <okthatsall678@gmail.com>
2025-03-15 23:12:22 +0545

    Folder tested

---
+++ BCT/test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("This folder is for BCT students.");
+    return 0;
+}
---
+++ BEI/test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("This folder is for BEI students.");
+    return 0;
+}
